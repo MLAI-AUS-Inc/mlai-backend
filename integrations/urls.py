676a2e7c6b64b3efdc41cd422a168b82f25abc04
Sam <samwise.donegan@gmail.com>
2025-12-16 17:08:16 +1100

    feat: Add endpoint to fetch Gmail subject lines and redirect Google callback to frontend.

--- integrations/urls.py
+++ integrations/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('connect/google', views.google_connect, name='google_connect'),
     path('callback/google', views.google_callback, name='google_callback'),
+    path('emails', views.get_gmail_emails, name='get_gmail_emails'),
     path('test/gmail', views.test_gmail_fetch, name='test_gmail_fetch'),
 ]
