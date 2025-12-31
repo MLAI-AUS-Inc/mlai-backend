a61534cbc94d6ad67477ad7183b1fa65627da039
drsamdonegan <samwise.donegan@gmail.com>
2025-12-31 15:50:14 +1100

    feat: Register UserIntegration and GoogleConnection models with Django admin, configuring their display, search, filter, and read-only fields.

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserIntegration, GoogleConnection
 
-# Register your models here.
+@admin.register(UserIntegration)
+class UserIntegrationAdmin(admin.ModelAdmin):
+    list_display = ('slack_user_id', 'github_user_name', 'github_repo', 'project_scanned', 'last_scanned_at', 'updated_at')
+    search_fields = ('slack_user_id', 'github_user_name', 'github_repo')
+    list_filter = ('project_scanned', 'updated_at')
+    readonly_fields = ('updated_at', 'last_scanned_at', 'last_scanned_sha')
+
+@admin.register(GoogleConnection)
+class GoogleConnectionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'google_email', 'updated_at')
+    search_fields = ('user__email', 'google_email')
