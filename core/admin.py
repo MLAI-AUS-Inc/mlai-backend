71de33d6f3144b3738e088b8671227dfe9d72e49
drsamdonegan <samwise.donegan@gmail.com>
2026-01-14 14:46:23 +1100

    updatign to handle competitors

--- core/admin.py
+++ core/admin.py
@@ -52,7 +52,7 @@ admin.site.register(User, UserAdmin)
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'domain', 'created_at')
+    list_display = ('name', 'domain', 'competitors', 'created_at')
     search_fields = ('name', 'domain')
     ordering = ('name',)
 
