e11d4d7d3dad81b8205ab42d0c80520877ea1b5f
drsamdonegan <samwise.donegan@gmail.com>
2026-03-23 17:55:07 +1100

    bugfix

---
+++ core/migrations/0035_rename_cf_run_workflow_status_idx_content_fac_workflo_10aee3_idx_and_more.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0034_add_content_factory_billing_fields"),
+    ]
+
+    operations = []
