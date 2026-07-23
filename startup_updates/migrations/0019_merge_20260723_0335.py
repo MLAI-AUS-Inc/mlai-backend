7c227cb41bd4504a99f11f3f6831f60899f13e67
drsamdonegan <samwise.donegan@gmail.com>
2026-07-23 13:19:18 +0930

    Merge startup update migration branches

---
+++ startup_updates/migrations/0019_merge_20260723_0335.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("startup_updates", "0017_monthly_update_visibility_publish_state"),
+        ("startup_updates", "0018_monthlyupdatereminderdelivery"),
+    ]
+
+    operations = []
